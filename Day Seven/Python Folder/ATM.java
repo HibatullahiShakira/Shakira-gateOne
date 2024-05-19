234d01e15148030b641d1b6c5bfe5764e9cb686a
HibatullahiShakira <Hibatullahishakira@gmail.com>
2024-05-19 01:29:58 +0100

    d

---
+++ Day Seven/Python Folder/ATM.java
@@ -0,0 +1,52 @@
+import random
+account_list = []
+balance = 0
+def create_account(first_name, secondName, pin, balance):
+	global balance 
+	account_details = []
+	account_number_to_string = str(account_number)
+	return f"Your account number as been created sucesfullly, Here is your details \n{first_name} {secondName} \nyour pin is{pin} \nAccount number: {account_number_to_string}"
+	account_details.append(first_name)
+	account_details.append(second_name)
+	account_details.append(pin)
+	account_details.append(account_number_to_string)
+	account_details.append(balance)
+	account_list.append(account_details)
+
+def balance:
+	return balance 
+	
+def closeAccount(account_number):
+	if account_number in account_details:
+		remove.account_details
+	return Account closed sucessfully
+
+def withdraw_money(amount):
+	global balance 
+	if amount > balance:
+		balance -= amount
+		return balance
+	else: 
+		return insufficient fund
+def deposit_money(amount):
+	global balance
+	balance += amount
+	return balance 
+
+def transfer(account_number_to, amount):
+	global balance 
+	if account_number_to in account_list:
+
+		balance -= amount
+		if 
+		balance += amount
+		print(account number validated)
+
+ 
+	
+	
+	
+
+
+
+	
